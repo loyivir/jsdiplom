12c77d12594c8b339a4d8a8afe0b79bf60f50064
loyivir <loyivir@gmail.com>
2021-05-13 23:19:28 +0300

    feat: Add modal window handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 'use strict';
+import modalCallback from './modules/modalCallback';
+modalCallback();
---
+++ src/modules/modalCallback.js
@@ -0,0 +1,45 @@
+//popup
+const modalCallback = () => {
+  const popup = document.querySelector('.modal-callback');
+  const overlay = document.querySelector('.modal-overlay');
+  popup.style.opacity = '0%';
+  overlay.style.opacity = '0%';
+  const hideModal = (modal) => {
+    modal.style.display = 'none';
+    modal.style.opacity = '0%';
+  };
+  hideModal(popup);
+  hideModal(overlay);
+  const showModal = (modal) => {
+    if (screen.width < 768) {
+      modal.style.opacity = '100%';
+      modal.style.display = 'block';
+      return;
+    }
+    const start = Date.now();
+    modal.style.display = 'block';
+    const requestId = setInterval(() => {
+      const timePassed = Date.now() - start;
+      modal.style.opacity = `${(timePassed / 3).toFixed(0)}%`;
+      if (timePassed >= 1000) {
+        clearInterval(requestId);
+        return;
+      }
+    }, 10);
+  };
+  document.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.contains('callback-btn') || target.classList.contains('button-services')) {
+      showModal(popup);
+      showModal(overlay);
+    } else if (target.closest('.modal-close')) {
+      hideModal(popup);
+      hideModal(overlay);
+    } else if (!target.closest('.modal-callback')) {
+      hideModal(popup);
+      hideModal(overlay);
+    }
+  });
+};
+
+export default modalCallback;
